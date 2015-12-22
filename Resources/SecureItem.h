7c178c368e9dbc3c997cf426e651b288a9b74ae9
Gordon Fontenot <gmoney@gitpushgitpaid.com>
2015-12-22 10:46:57 -0800

    Add initial project info

---
+++ Resources/SecureItem.h
@@ -0,0 +1,19 @@
+//
+//  SecureItem.h
+//  SecureItem
+//
+//  Created by Gordon Fontenot on 12/22/15.
+//  Copyright © 2015 thoughtbot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SecureItem.
+FOUNDATION_EXPORT double SecureItemVersionNumber;
+
+//! Project version string for SecureItem.
+FOUNDATION_EXPORT const unsigned char SecureItemVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SecureItem/PublicHeader.h>
+
+
